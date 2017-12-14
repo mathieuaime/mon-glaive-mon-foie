b814ba386ebf1d57f697ed3302570528939fec9b
Mathieu Aimé <aime.mathieu1@gmail.com>
2017-12-14 21:53:06 +0100

    Add dice validator

--- app/src/main/java/com/mgmf/monglaivemonfoie/validator/DiceValidator.java
+++ app/src/main/java/com/mgmf/monglaivemonfoie/validator/DiceValidator.java
@@ -1,7 +1,12 @@
 package com.mgmf.monglaivemonfoie.validator;
 
+import android.annotation.SuppressLint;
+
 import com.mgmf.monglaivemonfoie.exception.BadDiceException;
 import com.mgmf.monglaivemonfoie.model.Dice;
+import com.mgmf.monglaivemonfoie.util.DiceUtil;
+
+import java.util.Arrays;
 
 /**
  * Created by Mathieu on 06/12/2017.
@@ -9,14 +14,13 @@ import com.mgmf.monglaivemonfoie.model.Dice;
 
 public class DiceValidator {
 
+    @SuppressLint("NewApi")
     public static void validate(Dice... dices) {
-        for (Dice dice : dices) {
-            validate(dice);
-        }
+        Arrays.stream(dices).forEach(DiceValidator::validate);
     }
 
     private static void validate(Dice dice) {
-        if (dice.getValue() <= 0 || dice.getValue() > Dice.FACES) {
+        if (dice.getValue() <= 0 || dice.getValue() > DiceUtil.FACES) {
             throw new BadDiceException();
         }
     }
